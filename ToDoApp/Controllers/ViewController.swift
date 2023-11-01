ae05623e4c5057d2432b32bf2cfc87e284acfa4f
AltynbekDinmukhamed <a.dimash.a_03@mail.ru>
2023-11-02 01:56:09 +0600

    Creating and setting controller

--- ToDoApp/Controllers/ViewController.swift
+++ ToDoApp/Controllers/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: -Variables-
+    
+    //MARK: -LifeCycle-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    //MARK: -Functions-
+}
 
-
+//MARK: -Exntension-
+extension ViewController {
+    
 }
 
